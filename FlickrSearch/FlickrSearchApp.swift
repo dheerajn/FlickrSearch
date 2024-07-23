6f157b8c5f5a8820d215e96ee09e527cc5670f7b
Dheeru Neelam <dheerajneelam.10@gmail.coim>
2024-07-23 10:57:22 -0500

    Initial Commit

---
+++ FlickrSearch/FlickrSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrSearchApp.swift
+//  FlickrSearch
+//
+//  Created by LZU4481 on 7/23/24.
+//
+
+import SwiftUI
+
+@main
+struct FlickrSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
